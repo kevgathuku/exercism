259d8ad1056ccfea45b81c3f45437d961014d052
Kevin Ndung'u <kevgathuku@gmail.com>
2016-09-27 10:43:39 +0300

    Move list_ops comments to the interface file

--- ocaml/list-ops/list_ops.ml
+++ ocaml/list-ops/list_ops.ml
@@ -1,15 +1,9 @@
-(* let fold init:'acc -> f:('acc -> 'a -> 'acc) -> 'a list -> 'acc *)
-(* ~init is used as a named argument *)
-(* You need to include the ~ in the recursive function call to refer to individual arguments *)
 let rec fold ~init:acc ~f lst =
    match lst with
     [] -> acc
     | hd :: tl -> fold ~init:(f acc hd) ~f tl
 ;;
 
-(* Evaluates the list from the last element to the first. *)
-(* Returns elements in the order it received them *)
-(* These are no longer named arguments, but positional arguments *)
 let rec fold_right (acc: 'b) (f: 'a -> 'b -> 'b) (lst: 'a list) : 'b =
     match lst with
     [] -> acc
@@ -21,7 +15,6 @@ let length lst =
 ;;
 
 let reverse lst =
-    (* The first element is added to the end of the list, then the rest follow*)
     fold ~init:[] ~f:(fun acc x -> x :: acc) lst
 ;;
 
@@ -34,12 +27,9 @@ let filter ~f lst =
 ;;
 
 let append lst_a lst_b =
-    (* Appends elements in list_b to list_a and returns a new list *)
-    (* Starts with list_b elements, then prepends list_a elements, from last to first *)
     fold_right lst_b (fun x acc -> x :: acc) lst_a
 ;;
 
 let concat lst =
-    (* Merges two lists into one *)
     fold_right [] (fun x acc -> append x acc) lst
 ;;
