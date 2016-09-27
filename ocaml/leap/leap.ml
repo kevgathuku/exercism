a8a0fd80c0aa1174b06cf029ac92db4765d65923
Kevin Ndung'u <kevgathuku@gmail.com>
2016-09-27 11:18:18 +0300

    Refactor leap year checker
    - Remove redundant function. Use the mod operator directly

--- ocaml/leap/leap.ml
+++ ocaml/leap/leap.ml
@@ -1,8 +1,5 @@
-let year_mod num year =
-    year mod num = 0
-;;
-
 let leap_year year =
-    if not (year_mod 4 year) then false
-    else if (year_mod 100 year) && not (year_mod 400 year) then false else true
+    if not ((mod) year 4 = 0) then false
+    else if ((mod) year 100 = 0) && not ((mod) year 400 = 0) then false
+    else true
 ;;
