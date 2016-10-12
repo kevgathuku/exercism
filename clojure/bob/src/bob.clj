bbd4a1a58a4f0ec64b9f14d7fa8e45f2b7cc7628
Kevin Ndung'u <kevgathuku@gmail.com>
2016-10-12 23:26:33 +0300

    Clojure: Bob - Refactor
    Move isUpper check to its own function

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,12 +1,18 @@
 (ns bob
   (:require [clojure.string :as str]))
 
+(defn isUpper [message]
+  (and
+    (= (str/upper-case message) message)
+    (not= (str/lower-case message) message)
+  )
+)
+
 (defn response-for
   "Bob's responses to different statement"
   ([message]
     (if (str/blank? message) "Fine. Be that way!"
-      (if (and (= (str/upper-case message) message) (not= message (str/lower-case message)))
-        "Whoa, chill out!"
+      (if (isUpper message) "Whoa, chill out!"
         (if (str/ends-with? message "?") "Sure."
           "Whatever.")))
   )
