ec0d6ad1bc7a8c8c23d5e4fbc4085aec6ae6eecc
Kevin Ndung'u <kevgathuku@gmail.com>
2016-10-13 09:59:00 +0300

    Clojure: Define dna-map inside function

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -1,9 +1,8 @@
 (ns rna-transcription)
 
-(def dna-map {\G "C" \C "G" \T "A" \A "U"})
-
 (defn get-rna [char]
-  (or (dna-map char) (throw (AssertionError. "Invalid Data"))))
+  (let [dna-map {\G "C" \C "G" \T "A" \A "U"}]
+    (or (dna-map char) (throw (AssertionError. "Invalid Data")))))
 
 (defn to-rna
   "Transcribe A DNA strand to its RNA complement"
