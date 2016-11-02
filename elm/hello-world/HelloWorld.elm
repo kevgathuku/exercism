2564628cdba999e99d27ef0dc2cbf73fe2d317d6
Kevin Ndung'u <kevgathuku@gmail.com>
2016-11-02 13:31:31 +0300

    Elm: Refactor Hello World

--- elm/hello-world/HelloWorld.elm
+++ elm/hello-world/HelloWorld.elm
@@ -3,9 +3,4 @@ module HelloWorld exposing (..)
 
 helloWorld : Maybe String -> String
 helloWorld name =
-    case name of
-        Nothing ->
-            "Hello, World!"
-
-        Just name ->
-            "Hello, " ++ name ++ "!"
+    "Hello, " ++ Maybe.withDefault "World" name ++ "!"
