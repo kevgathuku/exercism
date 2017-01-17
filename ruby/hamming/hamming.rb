e29dfafb71ea42bb0a2f6f9b0744d86f6305945c
Kevin Ndung'u <kevgathuku@gmail.com>
2017-01-17 12:33:51 +0300

    Use shorthand unless to raise error in case of unequal lengths

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -4,9 +4,7 @@ end
 
 class Hamming
   def self.compute(first, second)
-    if first.size != second.size
-      raise ArgumentError, "String lengths don't match"
-    end
+    raise ArgumentError, "String lengths don't match" unless first.size == second.size
     first.chars.zip(second.chars).select{|pair|  pair[0] != pair[1] }.length
   end
 end
