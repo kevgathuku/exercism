bd8ba3924c694886b5f17f0a179398bd47648a7c
Kevin Ndung'u <kevgathuku@gmail.com>
2017-01-26 11:35:03 +0300

    Replace select..length with count

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -5,6 +5,6 @@ end
 class Hamming
   def self.compute(first, second)
     raise ArgumentError, "String lengths don't match" unless first.size == second.size
-    first.chars.zip(second.chars).select{|nuc1, nuc2|  nuc1 != nuc2 }.length
+    first.chars.zip(second.chars).count{|nuc1, nuc2|  nuc1 != nuc2 }
   end
 end
