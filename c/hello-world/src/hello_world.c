c16d8c11b694b0ce2d67892b618895c74928cc53
Kevin Ndung'u <kevgathuku@gmail.com>
2017-01-26 12:12:00 +0300

    Add C Hello World solution

---
+++ c/hello-world/src/hello_world.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#include "hello_world.h"
+
+void hello(char *buffer, const char *name)
+{
+   if (name == NULL)
+   {
+        // Copies the string to buffer
+        strcpy(buffer, "Hello, World!");
+   }
+   else
+   {
+        // sends formatted output to buffer
+        sprintf(buffer, "Hello, %s!", name);
+   }
+}
