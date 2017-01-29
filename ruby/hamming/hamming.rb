664012fc8c33a67a9c519ed388eeb1c6a0000328
Kevin Ndung'u <kevgathuku@gmail.com>
2017-01-29 13:23:09 +0300

    Refactor Ruby hamming: Use each_char over chars

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -5,6 +5,8 @@ end
 class Hamming
   def self.compute(first, second)
     raise ArgumentError, "String lengths don't match" unless first.size == second.size
-    first.chars.zip(second.chars).count{|nuc1, nuc2|  nuc1 != nuc2 }
+    # Prefer each_char, which returns an Enumerator
+    # chars converts the enumerator to an array
+    first.each_char.zip(second.each_char).count { |nuc1, nuc2| nuc1 != nuc2 }
   end
 end
