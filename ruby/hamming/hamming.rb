512b7948b69d5b8a11895089a741b792ce31ff14
Kevin Ndung'u <kevgathuku@gmail.com>
2017-02-13 01:13:54 +0300

    Ruby hamming: Use length rather than size

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -4,7 +4,7 @@ end
 
 class Hamming
   def self.compute(first, second)
-    raise ArgumentError, "String lengths don't match" unless first.size == second.size
+    raise ArgumentError, "String lengths don't match" unless first.length == second.length
     # Prefer each_char, which returns an Enumerator
     # chars converts the enumerator to an array
     first.each_char.zip(second.each_char).count { |nuc1, nuc2| nuc1 != nuc2 }
