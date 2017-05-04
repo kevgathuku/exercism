327b85f1e1a08c7a2c8121b6287de6976d10511f
Kevin Ndung'u <kevgathuku@gmail.com>
2017-05-04 18:00:29 +0300

    Haskell leap year solution simplification

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,13 +1,8 @@
 module LeapYear (isLeapYear) where
 
-
-isDivisibleBy4 :: Integer -> Bool
-isDivisibleBy4 year =
-  mod year 4 == 0
-
 isLeapYear :: Integer -> Bool
 isLeapYear year
-  | mod year 4 == 0 && mod year 100 == 0 && mod year 400 == 0 = True
-  | mod year 4 == 0 && mod year 100 == 0 && mod year 400 /= 0 = False
+  | mod year 400 == 0 = True
+  | mod year 100 == 0 = False
   | mod year 4 == 0 = True
   | otherwise = False
