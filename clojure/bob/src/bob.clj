a9e2b98e7441e85e76cfff267a0aaea94a13faeb
Kevin Ndung'u <kevgathuku@gmail.com>
2018-12-04 12:12:38 +0300

    Clojure: Update BOB solution to match v2

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,19 +1,23 @@
 (ns bob
   (:require [clojure.string :as str]))
 
-(defn isUpper [message]
+(defn is-upper [message]
   (and
     (= (str/upper-case message) message)
-    (not= (str/lower-case message) message)
-  )
-)
+    (not= (str/lower-case message) message)))
+
+
+(defn is-question [message]
+  (str/ends-with? message "?"))
+  
 
 (defn response-for
   "Bob's responses to different statement"
   ([message]
-    (if (str/blank? message) "Fine. Be that way!"
-      (if (isUpper message) "Whoa, chill out!"
-        (if (str/ends-with? message "?") "Sure."
-          "Whatever.")))
-  )
-)
+   (cond
+     (str/blank? message) "Fine. Be that way!"
+     (and (is-upper message) (is-question message)) "Calm down, I know what I'm doing!"
+     (is-upper message)   "Whoa, chill out!"
+     (is-question message) "Sure."
+     :else "Whatever.")))  
+
