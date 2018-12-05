0c148c1c7c2d782f595de4922dc7a04a058ea552
Kevin Ndung'u <kevgathuku@gmail.com>
2018-12-05 16:29:40 +0300

    Enable num-digits to take a single argument

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -2,9 +2,11 @@
   (:require [clojure.string :as str]))
 
 
-(defn num-digits [num count]
-  (if (< num 10) (inc count)
-    (num-digits (/ num 10) (inc count))))
+(defn num-digits 
+  ([num] (num-digits num 0))
+  ([num count]
+   (if (< num 10) (inc count)
+     (num-digits (/ num 10) (inc count)))))
 
 
 (defn own-digits [num]
@@ -16,7 +18,7 @@
   
 
 (defn armstrong? [num]
-  (let [count (num-digits num 0)
+  (let [count (num-digits num)
         digits (own-digits num)
         armstrong (reduce + (map (fn [digit] (raise digit count)) digits))]
     (= armstrong num)))    
