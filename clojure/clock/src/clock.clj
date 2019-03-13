c158ba85b4ed3fdf0c7260744c79cd95634c1d62
Kevin Ndung'u <kevgathuku@gmail.com>
2019-03-13 13:23:09 +0300

    Clojure: Complete clock solution

--- clojure/clock/src/clock.clj
+++ clojure/clock/src/clock.clj
@@ -1,26 +1,21 @@
-(ns clock)
+(ns clock
+  (:require [clojure.string :as string]))
 
 
 (defn process-hours [hours]
-  (if (< hours 0)
-    (->> (mod hours 24)
-         (format "%02d"))
-  
-    (->> (mod hours 12)
-         (format "%02d"))))
-  
+  (mod hours 24))
+
 
 (defn process-minutes [minutes]
-  (let [mins (->> (mod minutes 60)
-                  (format "%02d"))
+  (let [mins (mod minutes 60)
         extra-hours (quot minutes 60)]
     (if (< minutes 0)
       [mins (- extra-hours 1)]
       [mins extra-hours])))
 
 
-(defn clock->string [clock]
-  (str (first clock) ":" (second clock)))
+(defn clock->string [clock-instance]
+  (string/join ":" (map (partial format "%02d") clock-instance)))
 
 
 (defn clock [hours minutes]
@@ -29,6 +24,6 @@
     [processed-hours processed-minutes]))
 
 
-(defn add-time [clock time])
-
+(defn add-time [start-clock time]
+  (clock (first start-clock) (+ time (second start-clock))))
 
