5a66140e441e30c0a81c863c25c4e39576b1a04e
Kevin Ndung'u <kevgathuku@gmail.com>
2020-02-03 09:34:28 +0300

    CircleCI: Ensure a correct exit code when an individual test fails
    When a test for an individual exercise fails, ensure the whole test
    script fails
    e.g. if one of the Elixir exercise's tests fail, ensure the runtests
    script for Elixir fails

--- clojure/runtests.sh
+++ clojure/runtests.sh
@@ -1,1 +1,10 @@
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && lein test" \;
+#!/bin/bash
+
+set -e
+
+for path in `find . -maxdepth 1 -type d \( ! -name . \)`
+do
+  cd "$path"
+  pwd && lein test
+  cd ..
+done
