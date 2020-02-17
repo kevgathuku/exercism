73dcb3c18f625188300aba9ce1dafd304591b01a
Kevin Ndung'u <kevgathuku@gmail.com>
2020-02-17 11:10:35 +0300

    Elixir: Simplify Roman numerals solution

--- elixir/roman-numerals/lib/roman_numerals.ex
+++ elixir/roman-numerals/lib/roman_numerals.ex
@@ -1,19 +1,21 @@
 defmodule RomanNumerals do
   @mapping %{
-    1 => "I",
-    4 => "IV",
-    5 => "V",
-    9 => "IX",
-    10 => "X",
-    40 => "XL",
-    50 => "L",
-    90 => "XC",
-    100 => "C",
-    400 => "CD",
-    500 => "D",
-    900 => "CM",
-    1000 => "M"
-  }
+             1 => "I",
+             4 => "IV",
+             5 => "V",
+             9 => "IX",
+             10 => "X",
+             40 => "XL",
+             50 => "L",
+             90 => "XC",
+             100 => "C",
+             400 => "CD",
+             500 => "D",
+             900 => "CM",
+             1000 => "M"
+           }
+           |> Enum.to_list()
+           |> Enum.reverse()
 
   @doc """
   Convert the number to a roman number.
@@ -28,11 +30,6 @@ defmodule RomanNumerals do
   defp get_numeral(number, acc) do
     {candidate, roman} =
       @mapping
-      # same as Enum.reverse, but more explicit
-      # the mapping is now a list of items [{1, "I"},...]
-      |> Enum.sort(fn {key1, _roman1}, {key2, _roman2} -> key1 >= key2 end)
-      # We can also use >= directly, since tuples are compared element-by-element
-      # |> Enum.sort(&(&1 >= &2))
       |> Enum.find(fn {x, _roman} -> div(number, x) >= 1 end)
 
     get_numeral(number - candidate, acc <> roman)
