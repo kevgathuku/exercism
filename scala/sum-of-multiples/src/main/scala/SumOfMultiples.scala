18185d1259a0462a06974ce08b4a8a7a9c2765e5
Kevin Ndung'u <kevgathuku@gmail.com>
2020-02-17 16:44:39 +0300

    Scala: Update Sum of Multiples solution to latest Exercism version

--- scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,5 +1,5 @@
 object SumOfMultiples {
-  def sumOfMultiples(factors: Set[Int], limit: Int): Int = {
+  def sum(factors: Set[Int], limit: Int): Int = {
     val l = 1 until limit
     l.foldLeft(0)((x, y) => if (factors exists (factor => y % factor == 0)) x + y else x)
   }
