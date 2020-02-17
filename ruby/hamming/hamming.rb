e96f7e68e02345e76936b0a2bcfcef3fb062baa4
Kevin Ndung'u <kevgathuku@gmail.com>
2020-02-17 15:50:38 +0300

    Ruby: Update Hamming solution to latest exercism version

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -4,9 +4,9 @@ end
 
 class Hamming
   def self.compute(first, second)
-    raise ArgumentError, "String lengths don't match" unless first.length == second.length
+    raise ArgumentError, "String lengths don't match" unless first.size == second.size
     # Prefer each_char, which returns an Enumerator
     # chars converts the enumerator to an array
-    first.each_char.zip(second.each_char).count { |nuc1, nuc2| nuc1 != nuc2 }
+    first.each_char.zip(second.each_char).count{|nuc1, nuc2|  nuc1 != nuc2 }
   end
 end
