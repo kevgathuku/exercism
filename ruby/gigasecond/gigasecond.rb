ae5d4ece6e838b686694d19e3c2601868c4a5212
Kevin Ndung'u <kevgathuku@gmail.com>
2020-02-17 16:13:05 +0300

    Ruby: Add gigasecond solution

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -3,9 +3,9 @@ module BookKeeping
 end
 
 class Gigasecond
-  GIGASECOND = 1_000_000_000
+    GIGASECOND = 1_000_000_000
 
-  def self.from(moment)
-    moment + GIGASECOND
-  end
+    def self.from(moment)
+        moment + GIGASECOND
+    end
 end
