a55de4548b4b7357c94a93d013723faa0d0a2d25
Kevin Ndung'u <kevgathuku@gmail.com>
2020-02-27 11:13:32 +0300

    Elixir: Improve beer song solution
    Use pattern matching instead of guards for known numbers
    i.e. 0, 1, 2
    Use Enum.map_join to replace Enum.map & Enum.join

--- elixir/beer-song/lib/beer_song.ex
+++ elixir/beer-song/lib/beer_song.ex
@@ -3,31 +3,31 @@ defmodule BeerSong do
   Get a single verse of the beer song
   """
   @spec verse(integer) :: String.t()
-  def verse(number) when number > 2 do
+  def verse(0) do
     """
-    #{number} bottles of beer on the wall, #{number} bottles of beer.
-    Take one down and pass it around, #{number - 1} bottles of beer on the wall.
-    """
-  end
-
-  def verse(number) when number == 2 do
-    """
-    #{number} bottles of beer on the wall, #{number} bottles of beer.
-    Take one down and pass it around, #{number - 1} bottle of beer on the wall.
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.
     """
   end
 
-  def verse(number) when number == 1 do
+  def verse(1) do
     """
-    #{number} bottle of beer on the wall, #{number} bottle of beer.
+    1 bottle of beer on the wall, 1 bottle of beer.
     Take it down and pass it around, no more bottles of beer on the wall.
     """
   end
 
-  def verse(number) when number == 0 do
+  def verse(2) do
     """
-    No more bottles of beer on the wall, no more bottles of beer.
-    Go to the store and buy some more, 99 bottles of beer on the wall.
+    2 bottles of beer on the wall, 2 bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall.
+    """
+  end
+
+  def verse(number) when number > 2 do
+    """
+    #{number} bottles of beer on the wall, #{number} bottles of beer.
+    Take one down and pass it around, #{number - 1} bottles of beer on the wall.
     """
   end
 
@@ -36,6 +36,6 @@ defmodule BeerSong do
   """
   @spec lyrics(Range.t()) :: String.t()
   def lyrics(range \\ 99..0) do
-    Enum.map(range, &verse/1) |> Enum.join("\n")
+    Enum.map_join(range, "\n", &verse/1)
   end
 end
