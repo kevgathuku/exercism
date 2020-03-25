7c2bdf334c92ed9901c4e560b9edd15b510efe18
Kevin Ndung'u <kevgathuku@gmail.com>
2020-03-25 14:09:58 +0300

    Ruby: Simplify RNA Transcription solution

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -4,15 +4,13 @@ end
 
 class Complement
   CONVERSION_TABLE = {
-    'G' => 'C',
-    'C' => 'G',
-    'T' => 'A',
-    'A' => 'U'
+    "G" => "C",
+    "C" => "G",
+    "T" => "A",
+    "A" => "U",
   }.freeze
 
   def self.of_dna(strand)
-    rna = strand.each_char.map { |char| CONVERSION_TABLE[char] }
-    # If any of the chars is invalid i.e. returns nil, return an empty string
-    rna.all? ? rna.join : ''
+    strand.each_char.map { |char| CONVERSION_TABLE[char] }.join
   end
 end
