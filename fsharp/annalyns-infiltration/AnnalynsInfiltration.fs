85314bd91ba9e166075fa69e708b9ca8c94c6267
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2024-10-23 16:20:37 +0200

    Simplify some conditions

--- fsharp/annalyns-infiltration/AnnalynsInfiltration.fs
+++ fsharp/annalyns-infiltration/AnnalynsInfiltration.fs
@@ -3,22 +3,9 @@ module AnnalynsInfiltration
 let canFastAttack (knightIsAwake: bool) : bool = not knightIsAwake
 
 let canSpy (knightIsAwake: bool) (archerIsAwake: bool) (prisonerIsAwake: bool) : bool =
-    match knightIsAwake, archerIsAwake, prisonerIsAwake with
-    | false, false, false -> false
-    | true, false, false -> true
-    | false, true, false -> true
-    | false, false, true -> true
-    | false, true, true -> true
-    | true, false, true -> true
-    | true, true, false -> true
-    | true, true, true -> true
+    (knightIsAwake || archerIsAwake || prisonerIsAwake)
 
-let canSignalPrisoner (archerIsAwake: bool) (prisonerIsAwake: bool) : bool =
-    match archerIsAwake, prisonerIsAwake with
-    | false, true -> true
-    | true, false -> false
-    | true, true -> false
-    | false, false -> false
+let canSignalPrisoner (archerIsAwake: bool) (prisonerIsAwake: bool) : bool = (not archerIsAwake) && prisonerIsAwake
 
 let canFreePrisoner (knightIsAwake: bool) (archerIsAwake: bool) (prisonerIsAwake: bool) (petDogIsPresent: bool) : bool =
     match knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent with
