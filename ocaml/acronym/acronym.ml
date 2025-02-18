ae26ce2381dc4247c1056dec5387d555f2110e52
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2025-02-18 21:45:28 +0100

    Ocaml: simplify acronym solution

--- ocaml/acronym/acronym.ml
+++ ocaml/acronym/acronym.ml
@@ -1,11 +1,7 @@
 open Base
 
-let replace_dashes s =
-  String.map s ~f:(fun c -> if (Char.equal c '-') || (Char.equal c '_') then ' ' else c)
-
 let acronym phrase =
   phrase
-  |> replace_dashes
   |> String.uppercase
   |> String.split_on_chars ~on:[ ' '; '_'; '-' ]
   |> List.filter ~f:(fun x -> not (String.is_empty x))
