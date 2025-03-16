885dd6154d9c88e5827a82d0d37aa79c11293a4a
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2025-03-16 20:12:47 +0100

    Skip list-ops

--- ocaml/runtests.sh
+++ ocaml/runtests.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for path in `find . -maxdepth 1 -type d \( ! -name . \) ! -name '_*'`
+for path in `find . -maxdepth 1 -type d \( ! -name . \) ! -name '_*' ! -name 'list-ops'`
 do
   cd "$path"
   pwd && eval $(opam env) && make
