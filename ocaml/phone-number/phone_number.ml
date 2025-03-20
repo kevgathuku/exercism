35c23d7742a2f722d87e10241cf8adc4cd018379
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2025-03-20 20:35:23 +0100

    Ocaml: Add phone number solution

--- ocaml/phone-number/phone_number.ml
+++ ocaml/phone-number/phone_number.ml
@@ -1,2 +1,36 @@
-let number _ =
-    failwith "'number' is missing"
+open Base
+
+let contains_alpha (s : string) : bool = String.exists s ~f:Char.is_alpha
+
+let is_punctuation (c: char) : bool =
+  Char.equal c '@' || Char.equal c ':' || Char.equal c '!'
+
+let contains_punctuation (s: string): bool =
+  String.exists s ~f:is_punctuation
+
+let check_valid_first_digits (s: string) =
+  if Char.equal s.[0] '0' then
+    Error "area code cannot start with zero"
+  else if Char.equal s.[0] '1' then
+    Error "area code cannot start with one"
+  else if Char.equal s.[3] '0' then
+    Error "exchange code cannot start with zero"
+  else if Char.equal s.[3] '1' then
+    Error "exchange code cannot start with one"
+  else
+    Ok s
+
+let number candidate =
+  if contains_alpha candidate then
+    Error "letters not permitted"
+  else if contains_punctuation candidate then
+    Error "punctuations not permitted"
+  else
+    let extracted_num = String.filter candidate ~f:Char.is_digit in
+    match String.length extracted_num with
+    | 10 -> check_valid_first_digits extracted_num
+    | 11 when Char.equal extracted_num.[0] '1' ->
+        check_valid_first_digits (String.drop_prefix extracted_num 1)
+    | 11 -> Error "11 digits must start with 1"
+    | x when x > 11 -> Error "more than 11 digits"
+    | _ -> Error "incorrect number of digits"
