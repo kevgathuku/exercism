8ee2f467a633c69c1206f18de23621d207736b6b
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2025-05-11 22:59:38 +0200

    Ocaml: Remove duplication in fold accumulator function

--- ocaml/isogram/isogram.ml
+++ ocaml/isogram/isogram.ml
@@ -7,6 +7,6 @@ let is_isogram (word: string) : bool =
     else
     let cleaned_word = word |> String.lowercase |> String.filter ~f:Char.is_alpha in
     let chars_in_string (s : string) : CharSet.t =
-      String.fold s ~init:(Set.empty (module Char)) ~f:(fun acc ch -> Set.add acc ch)
+      String.fold s ~init:(Set.empty (module Char)) ~f:(Set.add)
     in
     String.length cleaned_word = Set.length (chars_in_string cleaned_word)
