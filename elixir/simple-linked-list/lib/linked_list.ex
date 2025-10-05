4dc550786c72e84c00364ab9dcb0c65b972954c9
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2025-10-05 22:36:42 +0200

    Elixir: refactor linked list solution

--- elixir/simple-linked-list/lib/linked_list.ex
+++ elixir/simple-linked-list/lib/linked_list.ex
@@ -6,56 +6,57 @@ defmodule LinkedList do
   """
   @spec new() :: t
   def new() do
-    {0, []}
+    {}
   end
 
   @doc """
   Push an item onto a LinkedList
   """
   @spec push(t, any()) :: t
-  def push({count, store}, elem), do: {count + 1, [elem] ++ store}
+  def push(lst, elem), do: {elem, lst}
 
   @doc """
   Counts the number of elements in a LinkedList
   """
   @spec count(t) :: non_neg_integer()
-  def count({len, _}), do: len
+  def count({}), do: 0
+  def count({_, tail}), do: 1 + count(tail)
 
   @doc """
   Determine if a LinkedList is empty
   """
   @spec empty?(t) :: boolean()
-  def empty?({0, _}), do: true
+  def empty?({}), do: true
   def empty?(_), do: false
 
   @doc """
   Get the value of a head of the LinkedList
   """
   @spec peek(t) :: {:ok, any()} | {:error, :empty_list}
-  def peek({0, _}), do: {:error, :empty_list}
+  def peek({}), do: {:error, :empty_list}
 
-  def peek({_, store}) do
-    {:ok, hd(store)}
+  def peek({head, _}) do
+    {:ok, head}
   end
 
   @doc """
   Get tail of a LinkedList
   """
   @spec tail(t) :: {:ok, t} | {:error, :empty_list}
-  def tail({0, _}), do: {:error, :empty_list}
+  def tail({}), do: {:error, :empty_list}
 
-  def tail({len, store}) do
-    {:ok, {len - 1, tl(store)}}
+  def tail({_, tl}) do
+    {:ok, tl}
   end
 
   @doc """
   Remove the head from a LinkedList
   """
   @spec pop(t) :: {:ok, any(), t} | {:error, :empty_list}
-  def pop({0, _}), do: {:error, :empty_list}
+  def pop({}), do: {:error, :empty_list}
 
-  def pop({len, store}) do
-    {:ok, hd(store), {len - 1, tl(store)}}
+  def pop({head, tail}) do
+    {:ok, head, tail}
   end
 
   @doc """
@@ -64,19 +65,24 @@ defmodule LinkedList do
   @spec from_list(list()) :: t
   def from_list([]), do: new()
 
-  def from_list(list), do: {Enum.count(list), list}
+  def from_list([head | tail]), do: {head, from_list(tail)}
 
   @doc """
   Construct a stdlib List LinkedList from a LinkedList
   """
   @spec to_list(t) :: list()
-  def to_list({_, store}), do: store
+  def to_list({}), do: []
+  def to_list({head, tail}), do: [head | to_list(tail)]
 
   @doc """
   Reverse a LinkedList
   """
   @spec reverse(t) :: t
-  def reverse({len, store}) do
-    {len, Enum.reverse(store)}
+  def reverse(list) do
+    reverse_helper(list, {})
   end
+
+  defp reverse_helper({}, acc), do: acc
+  # Insert the head as the first element in the acc, which starts off as an empty_list
+  defp reverse_helper({head, tail}, acc), do: reverse_helper(tail, {head, acc})
 end
