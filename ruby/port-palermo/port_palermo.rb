2be16bea23c83431eb5fffba4ea89da95cf5f410
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2026-09-24 14:23:53 +0200

    Ruby: add port palermo solution

---
+++ ruby/port-palermo/port_palermo.rb
@@ -0,0 +1,14 @@
+module Port
+  IDENTIFIER = :PALE
+
+  def self.get_identifier(city)
+    city[..3].upcase.to_sym
+  end
+
+  def self.get_terminal(ship_identifier)
+    cargo = ship_identifier.to_s[..2].downcase
+    return :A if %w[oil gas].include?(cargo)
+
+    :B
+  end
+end
